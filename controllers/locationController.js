496c13b621758a9cd1e3bb95f73b9f7ede64a2d3
Vadim <denba2013@yandex.ru>
2021-05-20 18:53:10 +0300

    fixes, add location list and location edit

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -1,17 +1,45 @@
 const {Location} = require('../models/models')
 
 class LocationController {
-    
-    async create(req,res){
-        const {l_ip,l_name,l_password,l_port,l_cores,l_ram} = req.body
 
-        const location = await Location.create({l_ip,l_name,l_password,l_port,l_cores,l_ram,l_status:1})
-        return res.json(location)
+    async create(req,res){
+        try{
+            const {l_ip,l_name,l_password,l_port,l_cores,l_ram} = req.body
+
+            const location = await Location.create({l_ip,l_name,l_password,l_port,l_cores,l_ram,l_status:1})
+            return res.json(location)
+        }catch(e){
+            return res.status(403).json({message:e})
+        }
     }
 
     async getAll(req,res) {
-        const locations = await Location.findAll()
-        return res.json(locations)
+        try{
+            const locations = await Location.findAll({where:{l_status:1}})
+            return res.json(locations)
+        }catch(e){
+            return res.status(403).json({message:e})
+        }
+    }
+
+    async getAllAdmin(req,res){
+        try{
+            const locations = await Location.findAll()
+            return res.json(locations)
+        }catch(e){
+            return res.status(403).json({message:e})
+        }
+    }
+
+    async editLocation(req,res){
+        try{
+            const {id} = req.params
+            const {l_ip,l_name,l_cores,l_ram,l_port} = req.body
+            const location = await Location.update({l_ip,l_name,l_cores,l_ram,l_port},{where:{id}})
+            return res.json({message:'Изменения сохранены'})
+        }catch(e){
+            return res.status(403).json({message:e})
+        }
     }
 
 }
--- routes/locationRouter.js
+++ routes/locationRouter.js
@@ -4,8 +4,10 @@ const checkRole = require('../middleware/checkRoleMiddleware')
 
 const router = new Router()
 
-router.post('/create',checkRole('ADMIN'),LocationController.create)
+router.post('/',checkRole('ADMIN'),LocationController.create)
 router.get('/',LocationController.getAll)
+router.get('/all',checkRole('ADMIN'),LocationController.getAllAdmin)
+router.put('/:id',checkRole('ADMIN'),LocationController.editLocation)
 
 
 module.exports  = router
